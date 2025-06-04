b889df2d9e88ebf33ec65ff8404a252bec8bebe2
chi-yh <chi802827@gmail.com>
2025-06-04 10:52:39 +0800

    商品數量按鈕調整

--- all.js
+++ all.js
@@ -10,3 +10,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const minusBtn = document.querySelector(".minusbtn");
+    const plusBtn = document.querySelector(".plusbtn");
+    const qtyInput = document.querySelector(".qty-input");
+
+    plusBtn.addEventListener("click", function () {
+    let current = parseInt(qtyInput.value) || 1;
+    qtyInput.value = current + 1;
+    });
+
+    minusBtn.addEventListener("click", function () {
+    let current = parseInt(qtyInput.value) || 1;
+    if (current > 1) {
+        qtyInput.value = current - 1;
+    }
+    });
+});
